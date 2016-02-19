a832987a2c1e917d81f5b4391d85a5e4640c5b7e
Serghei L <serghei.luchianov@icchange.ca>
2016-02-18 17:47:51 -0700

    Split away Buyer and Seller tables into separate sql script

--- SQL/BuyersAndSellersCRUD.sql
+++ SQL/BuyersAndSellersCRUD.sql
@@ -1,15 +1,57 @@
+Drop table if exists Buyer;
+-- Create the Buyer table
+Create Table Buyer
+(
+	BuyerID integer primary key AUTO_INCREMENT,
+    BidderNumber integer,
+    BuyerFirstName text,
+    BuyerLastName text,
+    BuyerAddress text,
+    BuyerCity text,
+    BuyerProvince text,
+    BuyerPostalCode text,
+    -- BuyerCountry Text,
+    BuyerPhone Text,
+    Permanent boolean default false,
+    Banned boolean default false
+);
+
+
+Drop table if exists Buyer;
+-- Create the Seller table
+Create Table Seller 
+(
+	SellerID integer primary key AUTO_INCREMENT,
+    SellerCode char(30),
+    SellerName text,
+    SellerStreet text,
+    SellerCity text,
+    SellerProvince text,
+    SellerPostalCode char(6),
+    -- SellerCountry text,
+    SellerPhone text,
+    SellerFax text,
+    ContactFirstName text,
+    ContactLastName text,
+    ConFile text,
+    DebtorFirstName text,
+    DebtorLastName text,
+    Private boolean,
+    GSTNumber text
+);
+
 DELIMITER $$
 DROP FUNCTION IF EXISTS ADD_BUYER $$
-Create FUNCTION ADD_BUYER (N_BUYER_FIRST_NAME text,
-	N_BUYER_LAST_NAME text,
-    N_BUYER_ADDRESS text,
-    N_BUYER_CITY text,
-    N_BUYER_PROVINCE text,
-    N_BUYER_POSTAL text,
-    N_BUYER_PHONE text,
-    N_BUYER_BIDNUM integer,
-    N_BUYER_PERM boolean,
-    N_BUYER_BANNED boolean) RETURNS INTEGER
+Create FUNCTION ADD_BUYER (N_BuyerFirstName text,
+	N_BuyerLastName text,
+    N_BuyerAddress text,
+    N_BuyerCity text,
+    N_BuyerProvince text,
+    N_BuyerPostalCode text,
+    N_BuyerPhone text,
+    N_BuyerBidderNumber integer,
+    N_BuyerPermanent boolean,
+    N_BuyerBanned boolean) RETURNS INTEGER
 
 begin    
     INSERT INTO `buyers`
@@ -24,119 +66,46 @@ begin
 `Banned` )
 VALUES
 (
-	N_BUYER_FIRST_NAME,
-	N_BUYER_LAST_NAME,
-    N_BUYER_ADDRESS,
-    N_BUYER_CITY,
-    N_BUYER_PROVINCE,
-    N_BUYER_POSTAL,
-    N_BUYER_PHONE,
-    N_BUYER_BIDNUM,
-    N_BUYER_PERM,
-    N_BUYER_BANNED)
+	N_BuyerFirstName,
+	N_BuyerLastName,
+    N_BuyerAddress,
+    N_BuyerCity,
+    N_BuyerProvince,
+    N_BuyerPostalCode,
+    N_BuyerPhone,
+    N_BuyerBidderNumber,
+    N_BuyerPermanent,
+    N_BuyerBanned)
 ;
     return LAST_INSERT_ID()
     ;
 end $$
 
 drop procedure if exists UPDATE_BUYER$$
-create procedure UPDATE_BUYER(N_BUYER_ID integer,
-N_BUYER_FIRST_NAME text,
-    N_BUYER_ADDRESS text,
-    N_BUYER_CITY text,
-    N_BUYER_PROVINCE text,
-    N_BUYER_POSTAL text,
-    N_BUYER_PHONE text,
-    N_BUYER_BIDNUM integer,
-    N_BUYER_PERM boolean,
-    N_BUYER_BANNED boolean)
+create procedure UPDATE_BUYER(N_BuyerID integer,
+N_BuyerFirstName text,
+	N_BuyerLastName text,
+    N_BuyerAddress text,
+    N_BuyerCity text,
+    N_BuyerProvince text,
+    N_BuyerPostalCode text,
+    N_BuyerPhone text,
+    N_BuyerBidderNumber integer,
+    N_BuyerPermanent boolean,
+    N_BuyerBanned boolean)
 begin
-	-- Must make sure that the change is trivial => if only buyer bidder number is changed we don't care
-	select 
-	@bname := `BuyerFirstName`,
-	@baddress := `BuyerAddress`,
-	@bcity := `BuyerCity`,
-	@bprovince := `BuyerProvince`,
-	@bpostal := `BuyerPostalCode`,
-	@bphone := `BuyerPhone`,
-	-- @bbidnum := `BidderNumber`,
-	@bperm := `Permanent`,
-	@bbanned := `Banned`
-	from buyers where buyers.BUYER_ID = N_BUYER_ID;
-
-	if 	(@bname = N_BUYER_FIRST_NAME) and 
-		(@baddress = N_BUYER_ADDRESS) and
-		(@bcity = N_BUYER_CITY) and
-		(@bprovince = N_BUYER_PROVINCE) and
-		(@bpostal = N_BUYER_POSTAL) and
-		(@bphone = N_BUYER_PHONE) and
-		(@bperm = N_BUYER_PERM) and
-		(@bbanned = N_BUYER_BANNED) then
-
-		UPDATE `buyers`
+	UPDATE `buyers`
 		SET
-		`BuyerFirstName` = N_BUYER_FIRST_NAME,
-		`BuyerAddress` = N_BUYER_ADDRESS,
-		`BuyerCity` = N_BUYER_CITY,
-		`BuyerProvince` = N_BUYER_PROVINCE,
-		`BuyerPostalCode` = N_BUYER_POSTAL,
-		`BuyerPhone` = N_BUYER_PHONE,
-		`BidderNumber` = N_BUYER_BIDNUM,
-		`Banned` = N_BUYER_BANNED
-		WHERE `BuyerID` = N_BUYER_ID;
-
-	elseif (select COUNT(SALE_ID) from sales where sales.BUYER_ID = N_BUYER_ID) > 0 then
-		UPDATE `buyers`
-		SET
-		`BUYER_ACTIVE` = FALSE
-		WHERE BUYER_ID = N_BUYER_ID;
-		INSERT INTO `buyers`
-		(`BuyerFirstName`,
-		`BuyerAddress`,
-		`BuyerCity`,
-		`BuyerProvince`,
-		`BuyerPostalCode`,
-		`BuyerPhone`,
-		`BidderNumber`,
-		`Permanent`,
-		`Banned`)
-		VALUES
-		(	N_BUYER_FIRST_NAME,
-			N_BUYER_ADDRESS,
-			N_BUYER_CITY,
-			N_BUYER_PROVINCE,
-			N_BUYER_POSTAL,
-			N_BUYER_PHONE,
-			N_BUYER_BIDNUM,
-			N_BUYER_PERM,
-			N_BUYER_BANNED);
-	ELSE
-		UPDATE `buyers`
-		SET
-		`BuyerFirstName` = N_BUYER_FIRST_NAME,
-		`BuyerAddress` = N_BUYER_ADDRESS,
-		`BuyerCity` = N_BUYER_CITY,
-		`BuyerProvince` = N_BUYER_PROVINCE,
-		`BuyerPostalCode` = N_BUYER_POSTAL,
-		`BuyerPhone` = N_BUYER_PHONE,
-		`BidderNumber` = N_BUYER_BIDNUM,
-		`Banned` = N_BUYER_BANNED
-		WHERE `BuyerID` = N_BUYER_ID;
-	end if;
+		`BuyerFirstName` = N_BuyerFirstName,
+		`BuyerAddress` = N_BuyerAddress,
+		`BuyerCity` = N_BuyerCity,
+		`BuyerProvince` = N_BuyerProvince,
+		`BuyerPostalCode` = N_BuyerPostalCode,
+		`BuyerPhone` = N_BuyerPhone,
+		`BidderNumber` = N_BuyerBidderNumber,
+		`Banned` = N_BuyerBanned
+		WHERE `BuyerID` = N_BuyerID;
 end;
-$$
-
-drop procedure if exists DEL_BUYER$$
-create procedure DEL_BUYER(N_BUYER_ID integer)
-if (select COUNT(SALE_ID) from sales where sales.BUYER_ID = N_BUYER_ID) > 0 then
-UPDATE `buyers`
-SET
-`BUYER_ACTIVE` = FALSE
-WHERE BUYER_ID = N_BUYER_ID;
-ELSE
-delete FROM BUYERS
-WHERE BUYER_ID = N_BUYER_ID;
-end if;
 $$
 DELIMITER ;
 
@@ -145,67 +114,66 @@ create procedure RESET_BUYER_BIDNUMS()
 UPDATE `buyers`
 SET
 `BidderNumber` = null
-WHERE BUYER_ACTIVE = TRUE AND BUYER_PERM = FALSE; 
+WHERE Permanent = FALSE; 
 
 drop procedure if exists GET_BUYERS;
 create procedure GET_BUYERS()
 SELECT
-BUYER_ID,
-BUYER_NAME,
-BUYER_ADDRESS,
-BUYER_CITY,
-BUYER_PROVINCE,
-BUYER_POSTAL,
-BUYER_PHONE,
-BUYER_BIDNUM,
-BUYER_PERM,
-BUYER_BANNED
-FROM `buyers`
-where BUYER_ACTIVE = TRUE;
+BuyerID,
+BuyerFirstName,
+BuyerAddress,
+BuyerCity,
+BuyerProvince,
+BuyerPostalCode,
+BuyerPhone,
+BidderNumber,
+Permanent,
+Banned
+FROM `buyers`;
 
 DELIMITER $$
 DROP FUNCTION IF EXISTS ADD_CON $$
-Create FUNCTION ADD_CON(N_CON_CODE text, 
-N_CON_NAME text, 
-N_CON_ADDRESS text, 
-N_CON_CITY text, 
-N_CON_PROVINCE text, 
-N_CON_POSTAL text, 
-N_CON_PHONE text, 
-N_OTH_PHONE text, 
-N_CON_FAX text, 
-N_CON_CONTACT text, 
-N_CON_FILE_NUMBER text,
+Create FUNCTION ADD_CON(N_SellerCode text, 
+N_SellerFirstName text, 
+N_SellerAddress text, 
+N_SellerCity text, 
+N_SellerProvince text, 
+N_SellerPostalCode text, 
+N_SellerPhone text, 
+N_SellerOtherPhone text, 
+N_SellerFax text, 
+N_ContactFirstName text, 
+N_SellerFileNumber text,
 N_CON_DL text,
-N_CON_GST boolean) RETURNS INTEGER
+N_GSTNumber boolean) RETURNS INTEGER
 begin
-        insert into CONS(CON_CODE, 
-        CON_NAME, 
-        CON_ADDRESS, 
-        CON_CITY, 
-        CON_PROVINCE, 
-        CON_POSTAL, 
-        CON_PHONE, 
-        OTH_PHONE, 
-        CON_FAX, 
-        CON_CONTACT, 
-        CON_FILE_NUMBER,
+        insert into CONS(SellerCode, 
+        SellerFirstName, 
+        SellerAddress, 
+        SellerCity, 
+        SellerProvince, 
+        SellerPostalCode, 
+        SellerPhone, 
+        SellerOtherPhone, 
+        SellerFax, 
+        ContactFirstName, 
+        SellerFileNumber,
         CON_DL,
-        CON_GST) 
+        GSTNumber) 
         
-        values (N_CON_CODE, 
-        N_CON_NAME, 
-        N_CON_ADDRESS, 
-        N_CON_CITY, 
-        N_CON_PROVINCE, 
-        N_CON_POSTAL, 
-        N_CON_PHONE, 
-        N_OTH_PHONE, 
-        N_CON_FAX, 
-        N_CON_CONTACT, 
-        N_CON_FILE_NUMBER,
+        values (N_SellerCode, 
+        N_SellerFirstName, 
+        N_SellerAddress, 
+        N_SellerCity, 
+        N_SellerProvince, 
+        N_SellerPostalCode, 
+        N_SellerPhone, 
+        N_SellerOtherPhone, 
+        N_SellerFax, 
+        N_ContactFirstName, 
+        N_SellerFileNumber,
         N_CON_DL,
-        N_CON_GST);
+        N_GSTNumber);
         return LAST_INSERT_ID();
         
 end $$
@@ -214,34 +182,34 @@ DELIMITER ;
 drop procedure if exists UPDATE_CON;
 create procedure UPDATE_CON(
 N_CON_ID integer, 
-N_CON_CODE text, 
-N_CON_NAME text, 
-N_CON_ADDRESS text, 
-N_CON_CITY text, 
-N_CON_PROVINCE text, 
-N_CON_POSTAL text, 
-N_CON_PHONE text,
-N_OTH_PHONE text, 
-N_CON_FAX text, 
-N_CON_CONTACT text, 
-N_CON_FILE_NUMBER text,
+N_SellerCode text, 
+N_SellerFirstName text, 
+N_SellerAddress text, 
+N_SellerCity text, 
+N_SellerProvince text, 
+N_SellerPostalCode text, 
+N_SellerPhone text,
+N_SellerOtherPhone text, 
+N_SellerFax text, 
+N_ContactFirstName text, 
+N_SellerFileNumber text,
 N_CON_DL text,
-N_CON_GST boolean)
+N_GSTNumber boolean)
 UPDATE `cons`
 SET
-`CON_CODE` = N_CON_CODE,
-`CON_NAME` = N_CON_NAME,
-`CON_ADDRESS` = N_CON_ADDRESS, 
-`CON_CITY` = N_CON_CITY, 
-`CON_PROVINCE` = N_CON_PROVINCE,
-`CON_POSTAL` = N_CON_POSTAL,
-`CON_PHONE` = N_CON_PHONE,
-`OTH_PHONE` = N_OTH_PHONE,
-`CON_FAX` = N_CON_FAX,
-`CON_CONTACT` = N_CON_CONTACT,
-`CON_FILE_NUMBER` = N_CON_FILE_NUMBER,
+`SellerCode` = N_SellerCode,
+`SellerFirstName` = N_SellerFirstName,
+`SellerAddress` = N_SellerAddress, 
+`SellerCity` = N_SellerCity, 
+`SellerProvince` = N_SellerProvince,
+`SellerPostalCode` = N_SellerPostalCode,
+`SellerPhone` = N_SellerPhone,
+`SellerOtherPhone` = N_SellerOtherPhone,
+`SellerFax` = N_SellerFax,
+`ContactFirstName` = N_ContactFirstName,
+`SellerFileNumber` = N_SellerFileNumber,
 CON_DL = N_CON_DL,
-CON_GST = N_CON_GST
+GSTNumber = N_GSTNumber
 WHERE `CON_ID` = N_CON_ID;
 
 drop procedure if exists DEL_CON;
@@ -253,17 +221,17 @@ DROP PROCEDURE IF EXISTS GET_CONS;
 Create Procedure GET_CONS()
     SELECT
 CON_ID,
-CON_CODE,
-CON_NAME,
-CON_ADDRESS, 
-CON_CITY, 
-CON_PROVINCE,
-CON_POSTAL,
-CON_PHONE,
-OTH_PHONE,
-CON_FAX,
-CON_CONTACT,
-CON_FILE_NUMBER 'CON_FILE#',
+SellerCode,
+SellerFirstName,
+SellerAddress, 
+SellerCity, 
+SellerProvince,
+SellerPostalCode,
+SellerPhone,
+SellerOtherPhone,
+SellerFax,
+ContactFirstName,
+SellerFileNumber 'CON_FILE#',
 CON_DL,
-CON_GST
+GSTNumber
 FROM `cons`;
