9b66b1829273bc851e1decd0c4d850b11cdbf414
duaneRoach <droach1@WA304-09.nait.ca>
2016-02-20 14:24:48 -0700

    Changes to Default

--- AMS/Default.aspx
+++ AMS/Default.aspx
@@ -10,11 +10,8 @@
         </asp:DropDownList>
             <br />
          <asp:ListBox ID="LBAuctionList" runat="server" OnSelectedIndexChanged="LBAuctionList_SelectedIndexChanged">
-             <asp:ListItem>September 1, 2015</asp:ListItem>
-             <asp:ListItem>August 1, 2015</asp:ListItem>
          </asp:ListBox>
     </div>
 
     
-
 </asp:Content>
--- AMS/Default.aspx.cs
+++ AMS/Default.aspx.cs
@@ -4,14 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
+using AMS.App_Code;
 
 namespace AMS
 {
     public partial class _Default : Page
     {
+
+        AuctionDAL auctionService = new AMS.App_Code.AuctionDAL();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            DataSet auctions = auctionService.findAuctions();
+            if (auctions.Tables[0].Rows.Count > 0)
+            {
+                //TXTAddress.Text = buyers.Tables["Buyers"].Rows[0]["BuyerName"].ToString();
+                //DDLBuyerName.DataTextField = "BuyerName";
+                //DDLBuyerName.DataValueField = "BuyerID";
+                //DDLBuyerName.DataSource = buyers;
+                //DDLBuyerName.DataBind();
+
+                LBAuctionList.DataSource = auctions;
+                LBAuctionList.DataTextField = "AuctionDate";
+                LBAuctionList.DataValueField = "AuctionId";
+                LBAuctionList.DataBind();
+
+            }
+            else
+            {
+
+            }
+
+
+
 
         }
 
