a19d4f36f7712849d0ef6789e96b3e1f06e01db5
Serghei L <serghei.luchianov@icchange.ca>
2016-03-06 09:50:08 -0700

    Support classes

---
+++ AMS/App_Code/SuppportClasses/Payment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMS.App_Code
+{
+    public class Payment
+    {
+        private int paymentID;
+
+        public int PaymentID
+        {
+            get { return paymentID; }
+            set { paymentID = value; }
+        }
+        private double paymentAmount;
+
+        public double PaymentAmount
+        {
+            get { return paymentAmount; }
+            set { paymentAmount = value; }
+        }
+        private int auctionSaleID;
+
+        public int AuctionSaleID
+        {
+            get { return auctionSaleID; }
+            set { auctionSaleID = value; }
+        }
+        private int paymentTypeID;
+
+        public int PaymentTypeID
+        {
+            get { return paymentTypeID; }
+            set { paymentTypeID = value; }
+        }
+        private double surcharges;
+
+        public double Surcharges
+        {
+            get { return surcharges; }
+            set { surcharges = value; }
+        }
+        private DateTime paymentDate;
+
+        public DateTime PaymentDate
+        {
+            get { return paymentDate; }
+            set { paymentDate = value; }
+        }
+    }
+}
