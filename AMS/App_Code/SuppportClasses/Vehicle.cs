10cd8e18f4154188920764e9680c0f39bf480e15
Grayson Maron <gmaron@apega.ca>
2016-03-10 15:49:19 -0700

    added vehicle class

--- AMS/App_Code/SuppportClasses/Vehicle.cs
+++ AMS/App_Code/SuppportClasses/Vehicle.cs
@@ -7,6 +7,162 @@ namespace AMS.App_Code.SuppportClasses
 {
     public class Vehicle
     {
+        private int vehicleID;
 
+        public int VehicleID
+        {
+            get { return vehicleID; }
+            set { vehicleID = value; }
+        }
+        private int lotNumber;
+        public int LotNumber
+        {
+            get
+            {
+                return lotNumber;
+            }
+
+            set
+            {
+                lotNumber = value;
+            }
+        }
+
+        private string year;
+          public string Year { 
+            get
+            {
+                return Year;
+            }
+
+    set
+            {
+                Year = value;
+            }
+        }
+        private string make;
+        public string Make
+        {
+            get
+            {
+                return Make;
+            }
+
+            set
+            {
+                Make = value;
+            }
+        }
+        private string model;
+        public string Model
+        {
+            get
+            {
+                return Model;
+            }
+
+            set
+            {
+                Model = value;
+            }
+        }
+
+        private string vin;
+        public string Vin
+        {
+            get
+            {
+                return Vin;
+            }
+
+            set
+            {
+                Vin = value;
+            }
+        }
+
+        private string color;
+        public string Color
+        {
+            get
+            {
+                return Color;
+            }
+
+            set
+            {
+                Color = value;
+            }
+        }
+
+        private int mileage;
+        public string Mileage
+        {
+            get
+            {
+                return Mileage;
+            }
+
+            set
+            {
+                Mileage = value;
+            }
+        }
+
+        private string units;
+        public string Units
+        {
+            get
+            {
+                return Units;
+            }
+
+            set
+            {
+                Units = value;
+            }
+        }
+
+        private string province;
+        public string Province
+        {
+            get
+            {
+                return Province;
+            }
+
+            set
+            {
+                Province = value;
+            }
+        }
+
+        private string transmission;
+        public string Transmission
+        {
+            get
+            {
+                return Transmission;
+            }
+
+            set
+            {
+                Transmission = value;
+            }
+        }
+
+        private string vehicleOptions;
+        public string VehicleOptions
+        {
+            get
+            {
+                return VehicleOptions;
+            }
+
+            set
+            {
+                VehicleOptions = value;
+            }
+        }
     }
 }
