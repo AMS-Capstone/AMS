0191524fb3f3bb0a0e525cdf1f810950b37aa6eb
Grayson Maron <gmaron@apega.ca>
2016-04-14 16:06:44 -0600

    change

--- AMS/App_Code/DAL/DataAction.cs
+++ AMS/App_Code/DAL/DataAction.cs
@@ -739,7 +739,7 @@ namespace AMS.App_Code
                     {
                         conn.Open();
                         cmd.CommandText = "sp_getVehicleByID";
-                        cmd.Parameters.AddWithValue("N_VehicleID", vehicleID);
+                        cmd.Parameters.AddWithValue("pVehicleID", vehicleID);
                         cmd.CommandType = CommandType.StoredProcedure;
                         using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
                         {
@@ -920,7 +920,7 @@ namespace AMS.App_Code
             {
                 MySqlCommand cmd = new MySqlCommand();
                 cmd.CommandText = "sp_getVehiclePicturesByVehicleID";
-                cmd.Parameters.Add(new MySqlParameter("@N_VehicleID", vehicleID));
+                cmd.Parameters.Add(new MySqlParameter("@pVehicleID", vehicleID));
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Connection = conn;
                 da.SelectCommand = cmd;
