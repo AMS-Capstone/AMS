063158bca95d92a17084698a8530eae593ce35b8
Serghei L <serghei.luchianov@icchange.ca>
2016-09-02 15:00:25 -0600

    Added auction bidder class

---
+++ AMS/App_Code/SuppportClasses/AuctionBidder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMS.App_Code
+{
+    public class AuctionBidder
+    {
+        private int auctionID;
+
+        public int AuctionID
+        {
+            get { return auctionID; }
+            set { auctionID = value; }
+        }
+        private int bidderNumber;
+
+        public int BidderNumber
+        {
+            get { return bidderNumber; }
+            set { bidderNumber = value; }
+        }
+
+        private int buyerID;
+
+        public int BuyerID
+        {
+            get { return buyerID; }
+            set { buyerID = value; }
+        }
+    }
+}
